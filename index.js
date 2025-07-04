700774e4e38492338d0270ffc81f085b804619bc
WayneMuongi <waynemuongi77@gmail.com>
2025-07-04 03:29:07 +0300

    edited

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded",function(){
+    console.log("The DOM has loaded");
+    
+});
+
+document.addEventListener("DOMContentLoaded", function(e){
+    let msg= document.getElementById("text")
+    msg.innerHTML=("This is really cool!")
+});
